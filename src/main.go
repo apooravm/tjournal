e457a1e3dcb74085ee670040b894b4911b615c7d
apooravm <medal.apoorav@gmail.com>
2024-01-18 18:21:59 +0530

    reset

--- src/main.go
+++ src/main.go
@@ -1,103 +1,66 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"log"
-	"os"
-
-	"github.com/charmbracelet/bubbles/textarea"
-	tea "github.com/charmbracelet/bubbletea"
+	"net/http"
 )
 
-type ConfigOptions struct {
+type LogReqPayload struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
+	Log      string `json:"log"`
 }
 
-const (
-	configFile = "./tjournal.config.json"
-)
+type JournError struct {
+	Err    error
+	Simple string
+}
 
-func checkConfigExists() (bool, error) {
-	if _, err := os.Stat(configFile); err == nil {
-		return true, nil
-	} else {
-		if os.IsNotExist(err) {
-			return false, nil
-		} else {
-			return false, err
-		}
-	}
+func (ce *JournError) Error() string {
+	return fmt.Sprintf("%v", ce.Simple)
 }
 
 func main() {
-
-	return
-
-	p := tea.NewProgram(initialModel())
-
-	if _, err := p.Run(); err != nil {
-		log.Fatal(err)
+	if err := sendPostReq("https://multi-serve.onrender.com/api/journal/", LogReqPayload{Username: "sb@nsAd*/", Password: "xxxxx", Log: "Day has been great"}); err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println("Logged! 🚀")
 	}
 }
 
-type errMsg error
-
-type model struct {
-	textarea textarea.Model
-	err      error
-}
-
-func initialModel() model {
-	ti := textarea.New()
-	ti.Placeholder = "How was your day..."
-	ti.Focus()
-
-	return model{
-		textarea: ti,
-		err:      nil,
-	}
-}
-
-func (m model) Init() tea.Cmd {
-	return textarea.Blink
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmds []tea.Cmd
-	var cmd tea.Cmd
-
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.Type {
-		case tea.KeyEsc:
-			if m.textarea.Focused() {
-				m.textarea.Blur()
-			}
-		case tea.KeyCtrlC:
-			return m, tea.Quit
-
-		default:
-			if !m.textarea.Focused() {
-				cmd = m.textarea.Focus()
-				cmds = append(cmds, cmd)
-			}
+func sendPostReq(url string, body interface{}) error {
+	// []byte
+	payload, err := json.Marshal(body)
+	if err != nil {
+		return &JournError{
+			Err:    err,
+			Simple: "Error marshalling object",
 		}
-
-	case errMsg:
-		m.err = msg
-		return m, nil
 	}
 
-	m.textarea, cmd = m.textarea.Update(msg)
-	cmds = append(cmds, cmd)
-	return m, tea.Batch(cmds...)
-}
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payload))
+	if err != nil {
+		return &JournError{
+			Err:    err,
+			Simple: "Error creating request",
+		}
+	}
 
-func (m model) View() string {
-	return fmt.Sprintf(
-		"Tell me a story.\n\n%s\n\n%s",
-		m.textarea.View(),
-		"(ctrl+c to quit)",
-	) + "\n\n"
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return &JournError{
+			Err:    err,
+			Simple: "Error sending request. ",
+		}
+	}
+
+	fmt.Println(res.Status)
+
+	res.Body.Close()
+	return nil
 }
